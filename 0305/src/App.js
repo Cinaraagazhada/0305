1b471ec970f859be3e8f92ad45ccfd195d2508f6
ChinaraAgha <126813954+Cinaraagazhada@users.noreply.github.com>
2023-05-03 05:45:30 +0400

    Add files via upload

---
+++ 0305/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+import Navbar from './Navbar';
+
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>Hello World!</p>
+        <h2>my first react application </h2>
+        <Navbar/>
+        <Footer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
